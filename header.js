2565a422e7dcead41cf657f63744a8c48b8802b8
Debora12122 <deboraoliveira2122@gmail.com>
2026-04-12 16:28:19 -0300

    Add files via upload

---
+++ header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>Meu Portfólio</h1>
+      <nav>
+        <a href="#projects">Projetos</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
